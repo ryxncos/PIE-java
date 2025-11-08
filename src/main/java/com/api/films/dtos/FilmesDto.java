a30a4bcf4cb55a3c1cbf8ec0a455d69da1be8e22
Ryan Silva <ryanclayton999@gmail.com>
2025-11-08 08:50:10 -0300

    Criação da funcionalidade de review

--- src/main/java/com/api/films/dtos/FilmesDto.java
+++ src/main/java/com/api/films/dtos/FilmesDto.java
@@ -1,5 +1,5 @@
 package com.api.films.dtos;
 
-public record FilmesDto(String titulo, String diretor, String genero, String anoLancamento, String sinopse, String elenco) {
+public record FilmesDto(String titulo, String diretor, String genero, String anoLancamento, String sinopse, String elenco, String review) {
 
 }
